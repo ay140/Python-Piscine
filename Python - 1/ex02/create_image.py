9f5090a31b7a6db8df4d8e8aa985aa630323ff17
Ayman Marzouk <amarzouk@lab2r1s8.42abudhabi.ae>
2026-02-17 21:56:45 +0400

    fix ex02

--- Python - 1/ex02/create_image.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import numpy as np
-
-# Create a simple valid image
-img = Image.fromarray(np.zeros((257, 450, 3), dtype=np.uint8), 'RGB')
-img.save("landscape.jpg")
-print("landscape.jpg created")
